65d612221f6b8a8d2f55ff8895b126e0956c16f1
网隐 <wangyin.yx@alibaba-inc.com>
2026-07-20 15:24:20 +0800

    feat(engine): support opt-in asynchronous scheduling

--- rtp_llm/cpp/models/PyWrappedModel.h
+++ rtp_llm/cpp/models/PyWrappedModel.h
@@ -5,6 +5,7 @@
 #include "rtp_llm/models_py/bindings/core/torch_utils/TypeConvert.h"
 #include <optional>
 #include <string>
+#include <atomic>
 #include <mutex>
 #include "rtp_llm/models_py/bindings/core/Types.h"
 #include "rtp_llm/models_py/bindings/core/DeviceData.h"
@@ -42,6 +43,9 @@ public:
     void            releaseBuffers() override;
     torch::Tensor   getMtpTargetHiddenStates(int64_t num_tokens) override;
     torch::Tensor   getMtpLastHiddenStates(int64_t num_tokens) override;
+    void            prepareAttentionInputs(const GptModelInputs& inputs) override;
+    void            prepareAttentionInputs(const GptModelInputs& inputs, bool skip_forward_event_sync);
+    void            updateKVCacheKernelBlockId(const GptModelInputs& inputs) override;
 
 private:
     std::optional<PyCacheStoreInputs> prepareWriteCacheParams(const GptModelInputs& inputs);
@@ -106,6 +110,11 @@ private:
     // is_pinned() is expensive on CPU; only assert during first N forwards as a sanity check.
     static constexpr int kPinnedCheckForwardCount = 3;
     int                  pinned_check_remaining_{kPinnedCheckForwardCount};
+
+    std::atomic<bool>                prepared_attention_inputs_{false};
+    torch_ext::PyAttentionInputs     attention_inputs_;
+    torch_ext::AttentionInputsByTag  attention_inputs_by_tag_;
+    CudaGraphState                   graph_state_;
 };
 
 // NOTE(wangyin): constructor can not be compiled correctly when placed in cc file.
